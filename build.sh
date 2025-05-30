9d8ba65ba846eed0c8d84a79262536acc4d1a851
Luke Brigan <lbbrigan@gmail.com>
2025-05-30 13:45:42 -0500

    simplify

--- build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf src/CommonLibrariesForNET/bin
-rm -rf src/CommonLibrariesForNET/obj
-rm -rf src/ChatterToolkitForNET/bin
-rm -rf src/ChatterToolkitForNET/obj
-rm -rf src/ForceToolkitForNET/bin
-rm -rf src/ForceToolkitForNET/obj
-
-dotnet clean src/ForceToolkitForNET.sln
-dotnet build src/ForceToolkitForNET.sln
--- push.sh
+++
@@ -1,3 +0,0 @@
-dotnet nuget push artifacts/DeveloperForce.Force.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
-
-dotnet nuget push artifacts/DeveloperForce.Chatter.$2.nupkg -k $1 -s https://api.nuget.org/v3/index.json
